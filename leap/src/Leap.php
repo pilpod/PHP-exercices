5c96906be11261a447d0e6cbf7a3ca47ae047429
pilpod <giacomo.corso@gmail.com>
2021-12-26 16:56:33 +0100

    exercise finished

--- leap/src/Leap.php
+++ leap/src/Leap.php
@@ -6,7 +6,23 @@ class Leap {
 
     function isLeap(int $year): bool
     {
-        return true;
+        $result = $this->isDivisibleBy($year);
+        return $result;
+    }
+
+    public function isDivisibleBy(int $year) : bool
+    {
+        $result = false;
+
+        if($year % 4 === 0) {
+            $result = true;
+        }
+
+        if($year % 100 === 0 && $year % 400 !== 0) {
+            $result = false;
+        }
+
+        return $result;
     }
 
 }
